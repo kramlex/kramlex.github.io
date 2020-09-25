2a5a5c5829fbc87b560a8c122822d0a23830e002
kramlex <mark.dubkov@gmail.com>
2020-09-25 21:47:33 +0700

    Updates

---
+++ precache-manifest.ba76f28e7dbad97ce938c6fb272798d6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7d3528699f628272b074e3357a7755",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0a3e4dce3e0dd3b3a90",
+    "url": "/static/css/main.07c36aee.chunk.css"
+  },
+  {
+    "revision": "9474e234ed255026f186",
+    "url": "/static/js/2.6f6fb4f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f6fb4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a3e4dce3e0dd3b3a90",
+    "url": "/static/js/main.b83e7bab.chunk.js"
+  },
+  {
+    "revision": "c3a28d83f34bd57df4d5",
+    "url": "/static/js/runtime-main.1b38b1d4.js"
+  },
+  {
+    "revision": "a38f2879275eb6af3619f79322572fd7",
+    "url": "/static/media/idea_off.a38f2879.png"
+  },
+  {
+    "revision": "889a2499606542104ae8c8ec4dfeac90",
+    "url": "/static/media/lamp_off.889a2499.png"
+  },
+  {
+    "revision": "26e103bd21cd97d17a4b5a2740322b29",
+    "url": "/static/media/lamp_on.26e103bd.png"
+  },
+  {
+    "revision": "1d33ebbd75fb634b60b98e94ea835f2d",
+    "url": "/static/media/light_line.1d33ebbd.png"
+  }
+]);
